bf2e44d23401ef3e89f5d2ef3eba6d30b04e40e9
James Kunstle <jkunstle@redhat.com>
2023-11-09 16:18:06 -0500

    go Ranger

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 	<p>
-	  Here's some new text
+	  Let's go Mets
+	</p>
+	<p>
+	  and Rangers
 	</p>
         <a
           className="App-link"
